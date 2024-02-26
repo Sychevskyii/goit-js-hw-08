f959fe96040d90294c40e74b9c8486cccfc80349
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-02-26 18:02:17 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,10 +5,17 @@ const player = new Player('vimeo-player', {
   width: 640,
 });
 
-player.on('timeupdate', function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-    let savedTime = localStorage.getItem('videoplayer-current-time');
-    currentTime = JSON.parse(savedTime);
-});
+player.on('timeupdate', saveCurrentTime);
 
-console.log(currentTime)
+function saveCurrentTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      
+    });
+  let savedTime = localStorage.getItem('videoplayer-current-time');
+  let currentTime = JSON.parse(savedTime);
+}
