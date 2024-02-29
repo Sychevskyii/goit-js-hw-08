5b1dd6ccdddbcc04eb7a8a7f840531843634eda8
Ivan <114999150+Sychevskyii@users.noreply.github.com>
2024-02-29 20:38:40 +0200

    update feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,9 +3,9 @@ import throttle from 'lodash-es/throttle';
 const FEEDBACK_KEY = 'feedback-form-state';
 
 const formEl = document.querySelector('.feedback-form');
-const inputEl = formEl[0];
-const textareaEl = formEl[1];
-const buttonEl = formEl[2];
+const inputEl = formEl.querySelector('input[name="email"]');
+const textareaEl = formEl.querySelector('textarea[name="message"]');
+const buttonEl = formEl.querySelector('button[type="submit"]');
 
 formEl.addEventListener('input', throttle(onSaveFormData, 500));
 buttonEl.addEventListener('click', onSubmitForm);
@@ -21,16 +21,16 @@ function onSaveFormData(e) {
 }
 
 const stringFormData = localStorage.getItem(FEEDBACK_KEY);
+let parsedFormData = {};
 
-const parsedFormData = JSON.parse(stringFormData);
-
-function setInputsValue() {
-  inputEl.value += parsedFormData.email;
-  textareaEl.value += parsedFormData.message;
+if (stringFormData !== null) {
+  parsedFormData = JSON.parse(stringFormData);
+  setInputsValue();
 }
 
-if (localStorage !== null) {
-  setInputsValue();
+function setInputsValue() {
+  inputEl.value = parsedFormData.email || '';
+  textareaEl.value = parsedFormData.message || '';
 }
 
 function onSubmitForm(e) {
